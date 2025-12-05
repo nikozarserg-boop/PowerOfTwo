94f08be510ee2766f503bcfd863adcbe06bd6fd2
nikozarserg-boop <nikozarserg@gmail.com>
2025-12-05 17:55:55 +0200

    Move Java files to src/main/java directory
    Renamed BigInt.java and PowerOfTwo.java to the src/main/java directory for better project structure. Also removed unused imports from PowerOfTwo.java.

--- Java/src/main/java/PowerOfTwo.java
+++ Java/src/main/java/PowerOfTwo.java
@@ -2,9 +2,6 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 public class PowerOfTwo {
     private static PrintWriter logFile;
